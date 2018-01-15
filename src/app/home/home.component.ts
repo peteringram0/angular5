54396d686aaa46cdb96bc9591cbd064ac0d1e5b0
Peter Ingram <p.ingram@managedracks.co.uk>
2018-01-15 20:42:40 +0000

    WIP

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
 
     api: string = 'https://www.reddit.com/r';
     url: string = 'watches';
-    items: object = [];
+    images: object = [];
 
     constructor(private http: HttpClient) {
     }
@@ -28,7 +28,7 @@ export class HomeComponent implements OnInit {
         if (this.url === null)
             return;
 
-        this.http.get(`${this.api}/${this.url}.json`)
+        this.http.get(`${this.api}/${this.url}.json?limit=100?sort=top&t=week`)
             .subscribe(this.processData.bind(this));
 
     }
@@ -41,9 +41,10 @@ export class HomeComponent implements OnInit {
         if (!feedData || !feedData.data)
             return;
 
-        this.items = feedData.data.children
+        this.images = feedData.data.children
             .map(post => post.data.url)
-            .filter(url => /jpg?$/.exec(url));
+            .filter(url => /gifv$/.exec(url))
+            .map(url => url.replace(/v$/, ''));
 
     }
 
